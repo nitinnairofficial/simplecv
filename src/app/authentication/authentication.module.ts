80fde8964e4bd430b5094234fa8e73b82969d9eb
nitin9nair <nitin9nair@gmail.com>
2020-07-21 14:25:23 +0530

    Added auth components

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { AuthenticationRoutingModule } from './authentication-routing.module';
-import { AuthenticationComponent } from './page/authentication/authentication.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-
+import { AuthenticationRoutingModule } from "./authentication-routing.module";
+import { AuthenticationComponent } from "./page/authentication/authentication.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [AuthenticationComponent, LoginComponent, SignupComponent, ForgotPasswordComponent],
-  imports: [
-    CommonModule,
-    AuthenticationRoutingModule
-  ]
+  declarations: [
+    AuthenticationComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+  ],
+  imports: [CommonModule, SharedModule, AuthenticationRoutingModule],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
